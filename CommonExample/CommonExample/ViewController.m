27fbe27916e1176a9086d3e100c6e78fbc98f172
何文学 <281519132@qq.com>
2020-07-01 18:05:12 +0800

    a

--- CommonExample/CommonExample/ViewController.m
+++ CommonExample/CommonExample/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import <ZLCommon/UIColor+JZDjHex.h>
+#import <ZLCommon/UIView+Toast.h>
 #import "TestView.h"
 
 @interface ViewController ()
@@ -18,11 +19,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithHexString:@"000000"];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    //self.view.backgroundColor = [UIColor colorWithHexString:@"000000"];
     
     TestView *test = [[TestView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     test.model = [UIColor redColor];
     [self.view addSubview:test];
+    
+    TestView *test1 = [[TestView alloc] initWithFrame:CGRectMake(test.left+30, test.bottom+30, 100, 100)];
+    test1.model = [UIColor blueColor];
+    [self.view addSubview:test1];
 }
 
 
